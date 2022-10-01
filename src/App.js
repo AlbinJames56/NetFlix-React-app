fe93cb077b1c28b30f8480f5eaf7f8ce6c7dea14
Albin James <albinjames56@gmail.com>
2022-10-02 01:07:22 +0530

    Basic complited

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
+import "./App.css";
+import Banners from "./Components/Banners/Banners";
+import NavBar from "./Components/NavBar/NavBar";
+import RowPost from "./Components/RowPost/RowPost";
+import { actions, originals } from "./urls";
 
-import './App.css';
-import Banners from './Components/Banners/Banners';
-import NavBar from './Components/NavBar/NavBar';
-import RowPost from './Components/RowPost/RowPost';
-
-function App() {
+function App(props) {
   return (
     <div className="App">
-      <NavBar/>
-       <Banners/>
-       <RowPost/>
-      
+      <NavBar />
+      <Banners />
+      <RowPost url={originals} title="Netflix Orginals" />
+      <RowPost url={actions} title="Actions" isSmall />
     </div>
   );
 }
--- src/Components/Banners/Banners.js
+++ src/Components/Banners/Banners.js
@@ -1,20 +1,37 @@
-import React from 'react'
-import "./Banners.css"
+import React, { useEffect, useState } from "react";
+import { API_KEY, imageUrl } from "../../Constants/Constants";
+import axios from "../../axios";
+import "./Banners.css";
 
 function Banners() {
+  const [movie, setMovie] = useState();
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.themoviedb.org/3/trending/all/week?api_key=${API_KEY}&language=en-US`
+      )
+      .then((Response) => {
+        setMovie(Response.data.results[0]);
+      });
+  }, []);
   return (
-    <div className='banners'>
-        <div className="content">
-            <h1 className='title'>Movie Name</h1>
-            <div className="banner-buttons">
-                <button className="button">Play</button>
-                <button className="button">My List</button>
-            </div>
-            <h1 className="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro dignissimos dolorem impedit?</h1>
+    <div
+      style={{
+        backgroundImage: `Url(${movie ? imageUrl + movie.backdrop_path : ""})`,
+      }}
+      className="banners"
+    >
+      <div className="content">
+        <h1 className="title">{movie ? movie.title : ""}</h1>
+        <div className="banner-buttons">
+          <button className="button">Play</button>
+          <button className="button">My List</button>
         </div>
-        <div className="fade_bottom"></div>
+        <h1 className="description">{movie ? movie.overview : ""}</h1>
+      </div>
+      <div className="fade_bottom"></div>
     </div>
-  )
+  );
 }
 
-export default Banners
+export default Banners;
